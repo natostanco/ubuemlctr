7f7132fad960729a84cc441ca359c35bff757f63
natostanco <bassobassista@Gmail.com>
2016-06-27 09:18:14 +0200

    .

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 /scripts/libinclude.sh
+
+hostname $HOSTNAME
+IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+echo $IP $DOMAIN $HOSTNAME >> /etc/hosts
+echo domain $DOMAIN >> /etc/resolv.conf
+chown clamav:clamav /var/log/mail/clamav.log
+find /var/run -name "*.pid" | xargs rm
+mkdir -p /usr/share/perl/5.18/Text
+ln -s /usr/share/perl/5.18.2/Text/ParseWords.pm /usr/share/perl/5.18/Text/ParseWords.pm
+
 /usr/local/bin/start-mailserver.sh 
