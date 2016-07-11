309090cba5830351df9ba7c0eeda6a99684000d3
Francesco Giannelli <bassobassista@gmail.com>
2016-07-11 05:04:45 +0000

    Update toucher.sh

--- scripts/toucher.sh
+++ scripts/toucher.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-touch `cat /host/list.txt`;
+echo "touching lists"
+touch `cat /host/list.txt`
+sleep 3
+touch `cat /host/manuallist.txt`
+sleep 3
+touch `cat /host/lddlist.txt`
+echo "done touching lists"
 
 /host/scripts/entry.sh
-
-
