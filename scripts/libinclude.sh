b26600d9773e8adc24c16fb36491fc8535904fb0
Francesco Giannelli <bassobassista@gmail.com>
2016-07-11 05:02:56 +0000

    Update libinclude.sh

--- scripts/libinclude.sh
+++ scripts/libinclude.sh
@@ -8,7 +8,16 @@ include=( "/usr/bin/post*" \
  
 for n in ${!include[*]}
 do
-find ${include[n]} | xargs -I {} ldd {} | awk '{print $3}' | grep '/' | sort -u | xargs -I {} touch {}
+find ${include[n]} | xargs -I {} ldd {} | awk '{print $3}' | grep '/' | sort -u | xargs -I {} echo {} >> /tmp/touch
 sleep 2
 echo "$n"
 done
+cat /tmp/touch | sort -u >> /host/manuallist.txt
+rm /tmp/touch
+
+while read in;
+do
+ldd $in 2>/dev/null | awk '{print $3}' | grep '/' | sort -u | xargs -I {} echo {} >> /tmp/touch
+done < /host/list.txt
+cat /tmp/touch | sort -u >> /host/lddlist.txt
+rm /tmp/touch
